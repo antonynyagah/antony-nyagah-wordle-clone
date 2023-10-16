e8a76207280429efac79f747338b3159b506796c
Antony Nyagah <antonynyagah@Antonys-MBP.lan>
2023-10-16 19:05:47 -0400

    added gameover component, mvp now complete

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -6,7 +6,7 @@ function Letter({letterPos, attemptVal}){
     const {board, correctWord, currAttempt, setDisabledLetters} = useContext(AppContext);
     const letter = board[attemptVal][letterPos]
 
-    const correct = correctWord[letterPos] === letter;
+    const correct = correctWord.toUpperCase()[letterPos] === letter;
     const almost = !correct && letter !== "" && correctWord.includes(letter)
     
     const letterState = currAttempt.attempt > attemptVal && ( correct ? "correct" : almost ? "almost" : "error") ;
