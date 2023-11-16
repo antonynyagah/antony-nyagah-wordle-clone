5fa8d64bbe500a529affc932cf1a7ab99a609906
Antony Nyagah <antonynyagah@Antonys-MBP.lan>
2023-11-15 22:59:52 -0500

    update 11/15

--- src/Words.js
+++ src/Words.js
@@ -1,3 +1,5 @@
+import wordBank from "./wordle-bank.txt";
+
 export const boardDefault = [
     ["", "", "", "", ""],
     ["", "", "", "", ""],
@@ -7,3 +9,15 @@ export const boardDefault = [
     ["", "", "", "", ""],
   ];
 
+  export const generateWordSet = async () => {
+    let wordSet;
+    let todaysWord;
+    await fetch(wordBank)
+      .then((response) => response.text())
+      .then((result) => {
+        const wordArr = result.split("\n");
+        todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)]
+        wordSet = new Set(wordArr);
+      });
+    return {wordSet, todaysWord};
+  };
